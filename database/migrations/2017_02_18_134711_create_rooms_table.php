09515a57818917ee2857696d538c224466a4f1f9
Juma Allan <allanjuma@gmail.com>
2017-02-18 19:11:06 +0300

    pull master

--- database/migrations/2017_02_18_134711_create_rooms_table.php
+++ database/migrations/2017_02_18_134711_create_rooms_table.php
@@ -15,7 +15,7 @@ class CreateRoomsTable extends Migration
     {
         Schema::create('rooms', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('id');
+            $table->string('number');
             $table->timestamps();
         });
     }
